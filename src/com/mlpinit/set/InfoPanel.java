9c5c03ba1db44aa621711cff7eba4d7318f3d676
Marius Pop <marius@mlpinit.com>
2017-04-04 00:33:45 +0300

    use GridBagLayout for InfoPanel

--- src/com/mlpinit/set/InfoPanel.java
+++ src/com/mlpinit/set/InfoPanel.java
@@ -5,12 +5,15 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
+import javax.swing.border.EmptyBorder;
 
+import java.awt.GridBagLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.BorderLayout;
 
 public class InfoPanel extends JPanel {
     private JLabel possibleSetsLabel;
@@ -18,20 +21,38 @@ public class InfoPanel extends JPanel {
     private JButton threeMoreButton;
 
     public InfoPanel() {
-        this.setLayout(new BorderLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        constraints.weightx = 1;
+        constraints.weighty = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+
+        GridBagLayout gridBagLayout = new GridBagLayout(); 
+        this.setLayout(gridBagLayout);
         this.setBackground(Color.gray);
         this.setPreferredSize(new Dimension(600, 120));
         this.possibleSetsLabel = createPossibleSetsJLabel();
         this.setsCountLabel = createSetsCountLabel();
-        this.add(possibleSetsLabel, BorderLayout.NORTH);
-        this.add(setsCountLabel, BorderLayout.CENTER);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        gridBagLayout.setConstraints(setsCountLabel, constraints);
+        this.add(possibleSetsLabel);
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        gridBagLayout.setConstraints(possibleSetsLabel, constraints);
+        this.add(setsCountLabel);
+        //
+        constraints.weighty = 3;
+        constraints.gridx = 0;
+        constraints.gridy = 2;
         this.threeMoreButton = new JButton("Add Three More");
+        gridBagLayout.setConstraints(threeMoreButton, constraints);
         threeMoreButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 frame().addThreeMoreCards();
             }
         });
-        this.add(threeMoreButton, BorderLayout.SOUTH);
+        this.add(threeMoreButton);
     }
 
     public void updatePossibleSetsLabel(int possibleSets) {
@@ -52,6 +73,7 @@ public class InfoPanel extends JPanel {
         JLabel label = new JLabel();
         label.setOpaque(true);
         label.setBackground(Color.gray);
+        label.setBorder(new EmptyBorder(10, 10, 10, 10));
         return label;
     }
     
@@ -59,6 +81,7 @@ public class InfoPanel extends JPanel {
         JLabel label = new JLabel("Sets found: 0.");
         label.setOpaque(true);
         label.setBackground(Color.gray);
+        label.setBorder(new EmptyBorder(10, 10, 10, 10));
         return label;
     }
 
